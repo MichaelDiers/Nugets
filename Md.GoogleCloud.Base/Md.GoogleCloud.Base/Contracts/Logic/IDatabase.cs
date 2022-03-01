cba4560ab2ccdc9e70c1924df8da1aad771cf9c0
Michael Diers <md@sreid.de>
2022-03-01 22:50:49 +0100

    Md.GoogleCloud.Base: add db

---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IDatabase.cs
@@ -0,0 +1,25 @@
+﻿namespace Md.GoogleCloud.Base.Contracts.Logic
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Access to the database.
+    /// </summary>
+    public interface IDatabase
+    {
+        /// <summary>
+        ///     Insert a new object to the database.
+        /// </summary>
+        /// <param name="documentId">The id of the document.</param>
+        /// <param name="data">The data to be saved.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        Task InsertAsync(string documentId, IToDictionary data);
+
+        /// <summary>
+        ///     Insert a new object to the database.
+        /// </summary>
+        /// <param name="data">The data to be saved.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        Task InsertAsync(IToDictionary data);
+    }
+}
---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Logic/DatabaseConfiguration.cs
@@ -0,0 +1,42 @@
+﻿namespace Md.GoogleCloud.Base.Logic
+{
+    using System;
+    using Md.GoogleCloud.Base.Contracts.Logic;
+
+    /// <summary>
+    ///     Configuration of the database.
+    /// </summary>
+    public class DatabaseConfiguration : IDatabaseConfiguration
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="DatabaseConfiguration" />.
+        /// </summary>
+        /// <param name="projectId">The id of the google cloud project.</param>
+        /// <param name="collectionName">The name of the collection.</param>
+        public DatabaseConfiguration(string projectId, string collectionName)
+        {
+            if (string.IsNullOrWhiteSpace(projectId))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(projectId));
+            }
+
+            if (string.IsNullOrWhiteSpace(collectionName))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(collectionName));
+            }
+
+            this.ProjectId = projectId;
+            this.CollectionName = collectionName;
+        }
+
+        /// <summary>
+        ///     Gets the name of the collection.
+        /// </summary>
+        public string CollectionName { get; }
+
+        /// <summary>
+        ///     Gets the id of the google cloud project.
+        /// </summary>
+        public string ProjectId { get; }
+    }
+}
