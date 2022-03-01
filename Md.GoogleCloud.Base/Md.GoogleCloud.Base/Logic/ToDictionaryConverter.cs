73a00be69bf20175a523f69d228edb02cd9a05bb
Michael Diers <md@sreid.de>
2022-03-01 22:54:20 +0100

    Md.GoogleCloud.Base: add db

---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Logic/ToDictionaryConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Md.GoogleCloud.Base.Logic
+{
+    using System.Collections.Generic;
+    using Md.GoogleCloud.Base.Contracts.Logic;
+
+    /// <summary>
+    ///     Implementation of <see cref="IToDictionary" />.
+    /// </summary>
+    public abstract class ToDictionaryConverter : IToDictionary
+    {
+        /// <summary>
+        ///     Add the property values to a dictionary.
+        /// </summary>
+        /// <param name="dictionary">The values are added to the given dictionary.</param>
+        /// <returns>The given <paramref name="dictionary" />.</returns>
+        public abstract IDictionary<string, object> AddToDictionary(IDictionary<string, object> dictionary);
+
+        /// <summary>
+        ///     Create a dictionary from the object properties.
+        /// </summary>
+        /// <returns>A <see cref="IDictionary{TKey,TValue}" />.</returns>
+        public IDictionary<string, object> ToDictionary()
+        {
+            return this.AddToDictionary(new Dictionary<string, object>());
+        }
+    }
+}
