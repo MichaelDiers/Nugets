e38fe54078d16e51a9fa461d8acdbd2a89f8ce00
Michael Diers <md@sreid.de>
2022-03-03 19:17:24 +0100

    Md.GoogleCloudFirestore: readonly database

--- Md.GoogleCloudFirestore/Md.GoogleCloudFirestore/Logic/ReadonlyDatabase.cs
+++ Md.GoogleCloudFirestore/Md.GoogleCloudFirestore/Logic/ReadonlyDatabase.cs
@@ -1,5 +1,8 @@
 ﻿namespace Md.GoogleCloudFirestore.Logic
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
     using Md.GoogleCloud.Base.Contracts.Logic;
 
     /// <summary>
@@ -14,6 +17,47 @@
         public ReadonlyDatabase(IDatabaseConfiguration databaseConfiguration)
             : base(databaseConfiguration)
         {
+        }
+
+        /// <summary>
+        ///     Read all entries of a database collection.
+        /// </summary>
+        /// <param name="fieldPath">Defines the field path.</param>
+        /// <param name="value">Defines the expected value of <paramref name="fieldPath" />.</param>
+        /// <returns>
+        ///     A <see cref="Task" /> whose is result is an <see cref="IEnumerable{T}" /> of
+        ///     <see cref="IDictionary{TKey,TValue}" />.
+        /// </returns>
+        public async Task<IEnumerable<IDictionary<string, object>>> ReadMany(string fieldPath, object value)
+        {
+            var snapshot = await this.Collection().WhereEqualTo(fieldPath, value).GetSnapshotAsync();
+            if (snapshot?.Any() == true)
+            {
+                return snapshot.Documents.Where(doc => doc.Exists).Select(doc => doc.ToDictionary()).ToArray();
+            }
+
+            return Enumerable.Empty<IDictionary<string, object>>();
+        }
+
+        /// <summary>
+        ///     Read an entry of a database collection.
+        /// </summary>
+        /// <param name="fieldPath">Defines the field path.</param>
+        /// <param name="value">Defines the expected value of <paramref name="fieldPath" />.</param>
+        /// <returns>A <see cref="Task" /> whose is result is a <see cref="IDictionary{TKey,TValue}" />.</returns>
+        public async Task<IDictionary<string, object>?> ReadOne(string fieldPath, object value)
+        {
+            var snapshot = await this.Collection().WhereEqualTo(fieldPath, value).Limit(1).GetSnapshotAsync();
+            if (snapshot?.Any() == true)
+            {
+                var documentSnapshot = snapshot.Documents.FirstOrDefault();
+                if (documentSnapshot?.Exists == true)
+                {
+                    return documentSnapshot.ToDictionary();
+                }
+            }
+
+            return null;
         }
     }
 }
