f2eade903bce13318b6b646292f50d3fac0cc45f
Michael Diers <md@sreid.de>
2022-03-03 19:14:57 +0100

    Md.GoogleCloud.Base: readonly database

--- Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IReadOnlyDatabase.cs
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IReadOnlyDatabase.cs
@@ -1,9 +1,30 @@
 ﻿namespace Md.GoogleCloud.Base.Contracts.Logic
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     /// <summary>
     ///     Readonly operations for databases.
     /// </summary>
     public interface IReadOnlyDatabase
     {
+        /// <summary>
+        ///     Read all entries of a database collection.
+        /// </summary>
+        /// <param name="fieldPath">Defines the field path.</param>
+        /// <param name="value">Defines the expected value of <paramref name="fieldPath" />.</param>
+        /// <returns>
+        ///     A <see cref="Task" /> whose is result is an <see cref="IEnumerable{T}" /> of
+        ///     <see cref="IDictionary{TKey,TValue}" />.
+        /// </returns>
+        Task<IEnumerable<IDictionary<string, object>>> ReadMany(string fieldPath, object value);
+
+        /// <summary>
+        ///     Read an entry of a database collection.
+        /// </summary>
+        /// <param name="fieldPath">Defines the field path.</param>
+        /// <param name="value">Defines the expected value of <paramref name="fieldPath" />.</param>
+        /// <returns>A <see cref="Task" /> whose is result is a <see cref="IDictionary{TKey,TValue}" />.</returns>
+        Task<IDictionary<string, object>?> ReadOne(string fieldPath, object value);
     }
 }
