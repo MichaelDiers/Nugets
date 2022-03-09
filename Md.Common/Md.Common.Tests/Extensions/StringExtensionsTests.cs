cf076e29e7ed2e834a58943ab157cce38e2509a1
Michael Diers <md@sreid.de>
2022-03-09 16:54:39 +0100

    Md.Common: initial checkin

---
+++ Md.Common/Md.Common.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Md.Common.Tests.Extensions
+{
+    using System;
+    using Md.Common.Extensions;
+    using Xunit;
+
+    /// <summary>
+    ///     Tests for <see cref="StringExtensions" />.
+    /// </summary>
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("invalid")]
+        [InlineData("00000000-0000-0000-0000-000000000000")]
+        public void GuidIsInvalid(string guid)
+        {
+            Assert.Throws<ArgumentException>(guid.ValidateIsAGuid);
+        }
+
+        [Fact]
+        public void GuidIsValid()
+        {
+            var guid = Guid.NewGuid().ToString();
+            Assert.Equal(guid, guid.ValidateIsAGuid());
+        }
+    }
+}
