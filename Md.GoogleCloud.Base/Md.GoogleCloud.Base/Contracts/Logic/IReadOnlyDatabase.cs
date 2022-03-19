ef07a68dbcf04fd2d7cfcd9683d0fb8edd23a92a
MichaelDiers <md@sreid.de>
2022-03-19 18:48:33 +0100

    Md.GoogleCloud.Base: database and sorting

--- Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IReadOnlyDatabase.cs
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IReadOnlyDatabase.cs
@@ -27,6 +27,22 @@
         Task<IEnumerable<IDictionary<string, object>>> ReadManyAsync(string fieldPath, object value);
 
         /// <summary>
+        ///     Read all entries of a database collection.
+        /// </summary>
+        /// <param name="fieldPath">Defines the field path.</param>
+        /// <param name="value">Defines the expected value of <paramref name="fieldPath" />.</param>
+        /// <param name="orderType">Specifies the sorting order.</param>
+        /// <returns>
+        ///     A <see cref="Task" /> whose is result is an <see cref="IEnumerable{T}" /> of
+        ///     <see cref="IDictionary{TKey,TValue}" />.
+        /// </returns>
+        Task<IEnumerable<IDictionary<string, object>>> ReadManyAsync(
+            string fieldPath,
+            object value,
+            OrderType orderType
+        );
+
+        /// <summary>
         ///     Read an entry of a database collection.
         /// </summary>
         /// <param name="fieldPath">Defines the field path.</param>
---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/OrderType.cs
@@ -0,0 +1,28 @@
+﻿namespace Md.GoogleCloud.Base.Contracts.Logic
+{
+    /// <summary>
+    ///     Specifies the sorting order.
+    /// </summary>
+    public enum OrderType
+    {
+        /// <summary>
+        ///     Value is not defined.
+        /// </summary>
+        Undefined = 0,
+
+        /// <summary>
+        ///     Do not sort the results.
+        /// </summary>
+        Unsorted = 1,
+
+        /// <summary>
+        ///     Sort in ascending order.
+        /// </summary>
+        Asc = 2,
+
+        /// <summary>
+        ///     Sort in descending order.
+        /// </summary>
+        Desc = 3
+    }
+}
