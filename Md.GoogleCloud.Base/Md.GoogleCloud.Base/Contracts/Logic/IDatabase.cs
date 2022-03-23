1409e55f77beaf23de116db1fa0947fec95ef6b5
MichaelDiers <md@sreid.de>
2022-03-23 15:16:29 +0100

    Md.GoogleCloud.Base: database fixes

--- Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IDatabase.cs
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IDatabase.cs
@@ -12,14 +12,14 @@
         /// </summary>
         /// <param name="documentId">The id of the document.</param>
         /// <param name="data">The data to be saved.</param>
-        /// <returns>A <see cref="Task" />.</returns>
-        Task InsertAsync(string documentId, IToDictionary data);
+        /// <returns>A <see cref="Task{T}" /> whose result is the document id.</returns>
+        Task<string> InsertAsync(string documentId, IToDictionary data);
 
         /// <summary>
         ///     Insert a new object to the database.
         /// </summary>
         /// <param name="data">The data to be saved.</param>
-        /// <returns>A <see cref="Task" />.</returns>
-        Task InsertAsync(IToDictionary data);
+        /// <returns>A <see cref="Task{T}" /> whose result is the document id.</returns>
+        Task<string> InsertAsync(IToDictionary data);
     }
 }
