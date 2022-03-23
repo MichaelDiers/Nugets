5d453e1e1209498f98e1b61639a458f36628ed2c
MichaelDiers <md@sreid.de>
2022-03-23 12:48:56 +0100

    Md.Common: extensions for enums fix

--- Md.Common/Md.Common.Tests/Extensions/EnumExtensionsTests.cs
+++ Md.Common/Md.Common.Tests/Extensions/EnumExtensionsTests.cs
@@ -11,14 +11,14 @@
         [InlineData(RegexOptions.Compiled)]
         public void IsDefined(RegexOptions options)
         {
-            Assert.Equal(options, options.IsDefined());
+            Assert.Equal(options, EnumExtensions.IsDefined(options, "foo"));
         }
 
         [Theory]
         [InlineData((RegexOptions) 100)]
         public void IsDefinedThrowsException(RegexOptions options)
         {
-            Assert.Throws<ArgumentException>(() => options.IsDefined());
+            Assert.Throws<ArgumentException>(() => options.IsDefined("foo"));
         }
     }
 }
