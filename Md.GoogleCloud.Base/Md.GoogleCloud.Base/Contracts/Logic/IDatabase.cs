b4236d056b484e09802911f8353d2aeb3bd1ea0c
MichaelDiers <md@sreid.de>
2022-03-23 22:40:18 +0100

    add updates to database

--- Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IDatabase.cs
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/IDatabase.cs
@@ -1,5 +1,6 @@
 ﻿namespace Md.GoogleCloud.Base.Contracts.Logic
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -21,5 +22,23 @@
         /// <param name="data">The data to be saved.</param>
         /// <returns>A <see cref="Task{T}" /> whose result is the document id.</returns>
         Task<string> InsertAsync(IToDictionary data);
+
+        /// <summary>
+        ///     Update a document by its document id.
+        /// </summary>
+        /// <param name="documentId">The id of the document.</param>
+        /// <param name="updates">The fields to be updated.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        Task UpdateByDocumentIdAsync(string documentId, IDictionary<string, object> updates);
+
+
+        /// <summary>
+        ///     Update a document that matches <paramref name="fieldPath" /> and <paramref name="value" />.
+        /// </summary>
+        /// <param name="fieldPath">The document is selected by the field path.</param>
+        /// <param name="value">The field path should be this value.</param>
+        /// <param name="updates">The updates for the document.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        Task UpdateOneAsync(string fieldPath, object value, IDictionary<string, object> updates);
     }
 }
