7f57b95c8a011a3ea6492bae5962df6cc1e27125
MichaelDiers <md@sreid.de>
2022-03-24 21:27:31 +0100

    Md.Common: json fix

--- Md.Common/Md.Common/Contracts/Environment.cs
+++ Md.Common/Md.Common/Contracts/Environment.cs
@@ -1,5 +1,7 @@
 ﻿namespace Md.Common.Contracts
 {
+    using System.Runtime.Serialization;
+
     /// <summary>
     ///     Specifies the runtime environment.
     /// </summary>
@@ -8,21 +10,25 @@
         /// <summary>
         ///     Undefined value.
         /// </summary>
+        [EnumMember(Value = "None")]
         None = 0,
 
         /// <summary>
         ///     Test environment.
         /// </summary>
+        [EnumMember(Value = "Test")]
         Test = 1,
 
         /// <summary>
         ///     Stage environment.
         /// </summary>
+        [EnumMember(Value = "Stage")]
         Stage = 2,
 
         /// <summary>
         ///     Prod environment.
         /// </summary>
+        [EnumMember(Value = "Prod")]
         Prod = 3
     }
 }
