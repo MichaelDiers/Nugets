bb1e08e99443034fe776fb62d9d2581210f94f95
MichaelDiers <md@sreid.de>
2022-04-03 22:02:34 +0200

    add env

---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/ISecretManagerEnvironment.cs
@@ -0,0 +1,13 @@
+﻿namespace Md.GoogleCloud.Base.Contracts.Logic
+{
+    /// <summary>
+    ///     Environment of the secret manager.
+    /// </summary>
+    public interface ISecretManagerEnvironment
+    {
+        /// <summary>
+        ///     Gets the id of the project.
+        /// </summary>
+        string ProjectId { get; }
+    }
+}
