e3531400e4313d464df41d95463a1d13891f2690
MichaelDiers <md@sreid.de>
2022-04-03 20:46:49 +0200

    add secret manager

---
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/ISecretManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Md.GoogleCloud.Base.Contracts.Logic
+{
+    /// <summary>
+    ///     Access the google cloud secrets manager.
+    /// </summary>
+    public interface ISecretManager
+    {
+        /// <summary>
+        ///     Read a secret by the given key.
+        /// </summary>
+        /// <param name="key">The key of the secret.</param>
+        /// <returns>The value for the given key.</returns>
+        string GetString(string key);
+    }
+}
