d860379ab8d33a06f96d55956d9b459aee621a35
MichaelDiers <md@sreid.de>
2022-04-04 23:21:31 +0200

    Md.GoogleCloud.Base: extend env

--- Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/ISecretManagerEnvironment.cs
+++ Md.GoogleCloud.Base/Md.GoogleCloud.Base/Contracts/Logic/ISecretManagerEnvironment.cs
@@ -1,10 +1,17 @@
 ﻿namespace Md.GoogleCloud.Base.Contracts.Logic
 {
+    using Md.Common.Contracts;
+
     /// <summary>
     ///     Environment of the secret manager.
     /// </summary>
     public interface ISecretManagerEnvironment
     {
+        /// <summary>
+        ///     Gets the environment.
+        /// </summary>
+        public Environment Environment { get; }
+
         /// <summary>
         ///     Gets the id of the project.
         /// </summary>
