268a9a7b839f83700bd5a71269babb644d7e3170
MichaelDiers <md@sreid.de>
2022-04-22 13:43:32 +0200

    validate project ids

---
+++ Md.Common/Md.Common.Tests/DataAnnotations/ProjectIdAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Md.Common.Tests.DataAnnotations
+{
+    using System.ComponentModel.DataAnnotations;
+    using Md.Common.DataAnnotations;
+    using Xunit;
+
+    public class ProjectIdAttributeTests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("a", true)]
+        [InlineData("a-", false)]
+        [InlineData("a-b", true)]
+        [InlineData("a-project-id", true)]
+        [InlineData("A-PROJECT", false)]
+        [InlineData("1project", false)]
+        public void Validate(string input, bool success)
+        {
+            var result = new ProjectIdAttribute().GetValidationResult(input, new ValidationContext(input));
+            if (success)
+            {
+                Assert.Equal(ValidationResult.Success, result);
+            }
+            else
+            {
+                Assert.NotEqual(ValidationResult.Success, result);
+            }
+        }
+    }
+}
---
+++ Md.Common/Md.Common/DataAnnotations/ProjectIdAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Md.Common.DataAnnotations
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    ///     Validator for project ids, like project-id.
+    /// </summary>
+    public class ProjectIdAttribute : RegularExpressionAttribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="ProjectIdAttribute" /> class.</summary>
+        public ProjectIdAttribute()
+            : base("^[a-z][a-z0-9]*(-[a-z][a-z0-9]*)*$")
+        {
+        }
+    }
+}
